89134517d72d516fd3b3af78e3935d2557dced1b
Murtin-java <marty.kaar@aon.at>
2020-05-12 16:19:24 +0200

    Added Vaper item as an image

--- dubaiRunnerGame/src/com/company/Verloren.java
+++ dubaiRunnerGame/src/com/company/Verloren.java
@@ -1,4 +1,25 @@
 package com.company;
 
+import java.util.Timer;
+import java.util.TimerTask;
 public class Verloren {
-}
+    Timer loose;
+    private int temp = 0, temp1 = 0;
+    public Verloren(){
+        loose = new Timer();
+        loose.scheduleAtFixedRate(new TimerTask(){
+@Override
+public void run() {
+              if(Var.verloren) if (temp >= 0 && temp <= 55) {
+                  temp++;
+              } else if (temp == 56) {
+                  System.out.println("Verloren");
+                  for (int i = 0; i < 5; i++) {
+                      Var.coinsX[i] = (int) (Math.random() * ((Var.screenWidth - 100) - 100) + 100);
+                      Var.coinsY[i] = -200 - temp;
+                      temp += 180;
+                  }
+
+              }}}, 0 ,15);
+    }
+        }
